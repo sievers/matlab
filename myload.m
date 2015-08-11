aebb1149b5b664abda0ca2a3227ed339f25e7d57
Jonathan Sievers <sievers@cita.utoronto.ca>
2015-08-11 04:06:05 -0400

    getting ready to pull more partitioned stuff

--- myload.m
+++ myload.m
@@ -1,6 +1,6 @@
 function [crud ] = myload(filename,varargin)
 head_skip=get_keyval_default('head_skip',0,varargin{:});
-
+comment_chars=get_keyval_default('comments','',varargin{:});
 % Load in an ascii file, but potentially with blank lines, comments, etc
 if iscell(filename)
   lines=filename;
@@ -10,6 +10,15 @@ end
 if head_skip>0
   lines=lines(head_skip+1:end);
 end
+
+for j=1:length(comment_chars),
+  for k=1:length(lines),
+    ll=lines{k};
+    ll(ll==comment_chars(j))=' ';
+    lines(k)=ll;
+  end
+end
+
 
 crap=sscanf(lines{1},'%f');
 crud=zeros(numel(lines),numel(crap));
